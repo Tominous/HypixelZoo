d7a15501c326b193765e136c8c567a500a9efd34
Eros <george.marr.cobc@gmail.com>
2018-01-08 13:35:12 +0000

    adding server

---
+++ server.py
@@ -0,0 +1,13 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+from http import HTTPStatus
+import shutil
+import posixpath
+import os
+import argparse
+import sys
+import urllib
+import html
+import io
+import subprocess
+from string import Template
+
