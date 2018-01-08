ceb1d2229a681e373d3ec30c8d12040a73e0c1df
Eros <george.marr.cobc@gmail.com>
2018-01-08 11:50:30 +0000

    added playing

--- __init__.py
+++ __init__.py
@@ -6,3 +6,14 @@ from twilio import twiml
 url = "https://www.youtube.com/watch?v=GNhbZ5guqO0&feature=youtu.be" #zoo url
 
 timeout = "Something went wrong. Woops."
+
+def play(tune):
+    if tune is None:
+        return
+
+    response = twiml.Response()
+
+    gather = response.gather(numDigits = 1, timeout = 10)
+    gather.playt(url)
+
+    return response
